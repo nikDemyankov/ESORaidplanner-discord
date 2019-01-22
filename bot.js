096909ec15d96c1e4cd0566d07cc737bce232727
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2019-01-22 09:39:17 +0100

    ♻️ Refactor command setter

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const Enmap = require('enmap');
 const R = require('ramda');
-const fs = require('fs');
 const events = require('./events');
+const commands = require('./commands');
 const config = require('./config.json');
 
 const client = new Discord.Client();
@@ -10,31 +10,27 @@ const client = new Discord.Client();
 client.config = config;
 
 client.on('ready', () => {
-  client.user.setActivity('planning for ' + client.guilds.size + ' guilds');
+  client.user.setActivity(`planning for ${client.guilds.size} guilds`);
   console.log('Bot ready!');
 });
 client.on('guildCreate', guild => {
-  client.user.setActivity('planning for ' + client.guilds.size + ' guilds');
+  client.user.setActivity(`planning for ${client.guilds.size} guilds`);
 });
 client.on('guildDelete', guild => {
-  client.user.setActivity('planning for ' + client.guilds.size + ' guilds');
+  client.user.setActivity(`planning for ${client.guilds.size} guilds`);
 });
 
+// set event handlers
 R.mapObjIndexed((eventHandler, eventName) => {
   client.on(eventName, eventHandler(client));
 }, events);
 
 client.commands = new Enmap();
 
-fs.readdir('./commands/', (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith('.js')) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split('.')[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
+// set command handlers
+R.mapObjIndexed((commandHandler, commandName) => {
+  console.log(`Attempting to load command ${commandName}: `, commandHandler);
+  client.commands.set(commandName, commandHandler);
+}, commands);
 
 client.login(config.token);
---
+++ commands/index.js
@@ -0,0 +1,17 @@
+const eventsCommand = require('./events');
+const helpCommand = require('./help');
+const setupCommand = require('./setup');
+const signoffCommand = require('./signoff');
+const signupCommand = require('./signup');
+const signupsCommand = require('./signups');
+const statusCommand = require('./status');
+
+module.exports = {
+  events: eventsCommand,
+  help: helpCommand,
+  setup: setupCommand,
+  signoff: signoffCommand,
+  signup: signupCommand,
+  signups: signupsCommand,
+  status: statusCommand,
+};
